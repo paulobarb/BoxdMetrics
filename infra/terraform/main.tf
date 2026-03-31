b6126aa44737df136695681be031f1ff61e3eead
Paulobarb <paulodgb2006@gmail.com>
2026-03-31 20:04:05 +0100

    configure s3 state backend and dynamodb lock

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,3 +1,7 @@
+provider "aws"{
+  region = "eu-north-1"
+}
+
 resource "aws_ecr_repository" "api" {
   name                 = "boxdmetrics-api"
   image_tag_mutability = "MUTABLE"
@@ -10,4 +14,34 @@ resource "aws_ecr_repository" "api" {
   image_scanning_configuration {
     scan_on_push = true
   }
+}
+
+resource "aws_s3_bucket" "terraform_state"{
+  bucket = "boxdmetrics-terraform-state-${random_id.suffix.hex}"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_versioning" "enabled" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name           = "boxdmetrics-terraform-locks"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+resource "random_id" "suffix" {
+  byte_length = 4
 }

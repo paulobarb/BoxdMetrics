cf1dc2277ca913ea9c5fa7bc9a2750e8aceb10eb
Paulobarb <paulodgb2006@gmail.com>
2026-04-02 20:17:48 +0100

    add terraform

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,6 +1,6 @@
-provider "aws"{
-  region = "eu-north-1"
-}
+# =========
+# == ECR ==
+# =========
 
 resource "aws_ecr_repository" "api" {
   name                 = "boxdmetrics-api"
@@ -16,12 +16,16 @@ resource "aws_ecr_repository" "api" {
   }
 }
 
+# =========
+# == S3 ==
+# =========
+
 resource "aws_s3_bucket" "terraform_state"{
   bucket = "boxdmetrics-terraform-state-${random_id.suffix.hex}"
 
-  lifecycle {
-    prevent_destroy = true
-  }
+  #lifecycle {
+    #prevent_destroy = true
+  #}
 }
 
 resource "aws_s3_bucket_versioning" "enabled" {
@@ -30,6 +34,10 @@ resource "aws_s3_bucket_versioning" "enabled" {
     status = "Enabled"
   }
 }
+
+# ==============
+# == DynamoDB ==
+# ==============
 
 resource "aws_dynamodb_table" "terraform_locks" {
   name           = "boxdmetrics-terraform-locks"
@@ -44,4 +52,211 @@ resource "aws_dynamodb_table" "terraform_locks" {
 
 resource "random_id" "suffix" {
   byte_length = 4
+}
+
+# =========
+# == ECS ==
+# =========
+resource "aws_ecs_cluster" "main"{
+  name  = "boxdmetrics-cluster"
+
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
+}
+
+# ===================
+# == ECS Task Role ==
+# ===================
+
+resource "aws_iam_role" "ecs_execution_role" {
+  name = "boxdmetrics-ecs-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = "TrustECS"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_execution_role_policy" {
+  role       = aws_iam_role.ecs_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role" "ecs_task_role" {
+  name = "boxdmetrics-ecs-task-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+    }]
+  })
+}
+
+# ==================
+# == ECS Task Def ==
+# ==================
+
+resource "aws_ecs_task_definition" "api" {
+  family                   = "boxdmetrics-api"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = 256
+  memory                   = 512
+
+  execution_role_arn = aws_iam_role.ecs_execution_role.arn
+  task_role_arn      = aws_iam_role.ecs_task_role.arn
+
+  container_definitions = jsonencode([
+    {
+      "name": "boxdmetrics-api",
+      "image": "${aws_ecr_repository.api.repository_url}:latest",
+      "essential": true
+    
+      portMappings = [
+        {
+          containerPort = 8000
+          hostPort      = 8000
+          protocol      = "tcp"
+        }
+      ]
+
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          "awslogs-group"         = aws_cloudwatch_log_group.api.name
+          "awslogs-region"        = "eu-north-1"
+          "awslogs-stream-prefix" = "ecs"
+        }
+      }
+    }
+  ])
+
+  runtime_platform {
+    operating_system_family = "LINUX"
+    cpu_architecture        = "X86_64"
+  }
+}
+
+# =========
+# == VPC ==
+# =========
+
+resource "aws_vpc" "main_vpc" {
+  cidr_block           = "10.10.10.0/24"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+}
+
+# ========
+# == GW ==
+# ========
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main_vpc.id
+}
+
+# =============
+# == Subnets ==
+# =============
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.10.10.0/25"
+  map_public_ip_on_launch = true
+  availability_zone = "eu-north-1a"
+}
+
+# ============
+# == Routes ==
+# ============
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+# ==============
+# == Firewall ==
+# ==============
+
+resource "aws_security_group" "web_sg" {
+  vpc_id = aws_vpc.main_vpc.id
+  name   = "boxdmetrics-web-sg"
+
+
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow HTTP from anywhere
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow all outbound traffic
+  }
+}
+
+# ================
+# == CloudWatch ==
+# ================
+
+resource "aws_cloudwatch_log_group" "api"{
+  name              = "/ecs/boxdmetrics-api"
+  retention_in_days = 7
+}
+
+# =================
+# == ECS Service ==
+# =================
+
+resource "aws_ecs_service" "api" {
+  name             = "boxdmetrics-api-service"
+  cluster          = aws_ecs_cluster.main.id
+  task_definition  = aws_ecs_task_definition.api.arn
+  desired_count    = 1
+  launch_type      = "FARGATE"
+  platform_version = "LATEST"
+
+  network_configuration {
+    subnets          = [aws_subnet.public_subnet.id] 
+    assign_public_ip = true
+    security_groups  = [aws_security_group.web_sg.id]
+  }
+
+  deployment_controller {
+    type = "ECS"
+  }
+
+  deployment_maximum_percent         = 200
+  deployment_minimum_healthy_percent = 100
+
+  wait_for_steady_state = false
 }

41fc4ffac8b972174ae85b9c9655d0039a2c2dea
Paulobarb <paulodgb2006@gmail.com>
2026-04-05 20:00:42 +0100

    migrate yaml to tf

---
+++ infra/terraform/oidc.tf
@@ -0,0 +1,86 @@
+# =======================================
+# == GitHub OIDC Identity Provider     ==
+# =======================================
+
+data "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+}
+
+# ===================================
+# == GitHub Actions IAM Role       ==
+# ===================================
+
+resource "aws_iam_role" "github_actions" {
+  name = "GitHub_Actions_Role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = data.aws_iam_openid_connect_provider.github.arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" : "repo:paulobarb/BoxdMetrics:*"
+          }
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" : "sts.amazonaws.com"
+          }
+        }
+      }
+    ]
+  })
+}
+
+# ==========================================
+# == Deployment Permissions Policy ==
+# ==========================================
+
+resource "aws_iam_role_policy" "github_actions_policy" {
+  name = "github-actions-deploy-policy"
+  role = aws_iam_role.github_actions.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AllowECRAuth"
+        Effect = "Allow"
+        Action = [
+          "ecr:GetAuthorizationToken"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "AllowECRUpload"
+        Effect = "Allow"
+        Action = [
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:GetRepositoryPolicy",
+          "ecr:DescribeRepositories",
+          "ecr:ListImages",
+          "ecr:DescribeImages",
+          "ecr:BatchGetImage",
+          "ecr:InitiateLayerUpload",
+          "ecr:UploadLayerPart",
+          "ecr:CompleteLayerUpload",
+          "ecr:PutImage"
+        ]
+        Resource = aws_ecr_repository.api.arn
+      },
+      {
+        Sid    = "AllowECSRestart"
+        Effect = "Allow"
+        Action = [
+          "ecs:UpdateService",
+          "ecs:DescribeServices"
+        ]
+        Resource = aws_ecs_service.api.id
+      }
+    ]
+  })
+}

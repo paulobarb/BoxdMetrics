ceacac31cdc80d35640acbbad8b74318745cbb8a
Paulobarb <paulodgb2006@gmail.com>
2026-04-09 17:41:19 +0100

    final3

--- src/backend/main.py
+++ src/backend/main.py
@@ -5,8 +5,8 @@ import logging
 from mangum import Mangum
 #from contextlib import asynccontextmanager
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from prometheus_fastapi_instrumentator import Instrumentator
+#from fastapi.middleware.cors import CORSMiddleware
+#from prometheus_fastapi_instrumentator import Instrumentator
 from slowapi import _rate_limit_exceeded_handler
 from slowapi.errors import RateLimitExceeded
 
